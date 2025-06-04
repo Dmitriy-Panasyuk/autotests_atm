0f9516dca0af9999d90ee5b70e7733bd95283600
d.panasyuk <ds91public@gmail.com>
2025-06-04 13:43:13 +0400

    fix allure path in config

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -8,7 +8,7 @@ public class Config {
    public static String PLATFORM_AND_BROWSER = "win_chrome";
    public static String WIN_ALLURE_RESULTS_PATH = (new File(AllureTools.class.getProtectionDomain().getCodeSource().getLocation().getFile())).getParent().replace("\\target","")+ "\\allure-results";
    public static String WIN_ALLURE_HTML_PATH = WIN_ALLURE_RESULTS_PATH + "\\ReportHTML";
-   public static String WIN_ALLURE_REPORT_PATH = "C:\\***\\Prog\\allure-2.29.0\\bin\\allure";
+   public static String WIN_ALLURE_REPORT_PATH = "C:\\Program Files\\allure-2.30.0\\bin\\allure";
    public static String LINUX_ALLURE_RESULTS_PATH = "/app/allure-results";
    public static String LINUX_ALLURE_HTML_PATH = LINUX_ALLURE_RESULTS_PATH + "/ReportHTML";
    public static String LINUX_ALLURE_REPORT_PATH = "/opt/allure-2.29.0/bin/allure";
